7d46d8a87e9d4ac7fa32e23e7608072d83b7b793
Vitor Sousa <vss2@cin.ufpe.br>
2020-03-30 16:05:24 -0300

    Arquivo GerarDados.py agora funciona, README atualizado, Main.py renomeado para main.py

--- GerarDados.py
+++ GerarDados.py
@@ -1,17 +1,15 @@
-
-# Importando duas bibliotecas, de variacao uniforme
-# e biblioteca de random inteiro
+# Importando quatro bibliotecas, 
+# de variacao uniforme
+# de biblioteca de random inteiro
+# de datas
+# de arquivos
 
 from random import uniform
 from random import randint
-
-
-# Importando biblioteca de diretorio atual
-
+import datetime
 import os
 
 texto = []
-
 
 # Gerador de 100 valores de altura e peso
 # A altura varia de 1.5 a 2.2
@@ -19,21 +17,43 @@ texto = []
 # Temos um inteiro aleatorio de 0 a 10
 
 def obterTexto():
-      for x in range[1,100]:
-            texto.append(str(uniform(1.50, 2.20)) + ';' +  
-            str(inform(50, 120)) + ';' + str(randint(0, 10)) + '\n')
+      for x in range(1,100):
+            texto.append(str(uniform(1.50, 2.20)) + ';\t' + str(uniform(50, 120)) + ';\t' + str(randint(0, 10)) + '\n')
+      return texto
 
 
 # Pegamos o diretorio
 # Damos a entrada a o lugar de gravar, permissoes, e codificacao
 # O codigo de diretorio foi corrigido para aceitar Windows, Mac e Linux
 
+def criarArquivo(ano, mes, dia, hor, minu):
+      try: 
+            nomeArquivo = ano+mes+dia+hor+minu+'.txt'
+            arquivoAberto = open(nomeArquivo, 'w+')
+            return arquivoAberto
+      except IOError:
+            deletar = input('Já existe um arquivo com o mesmo nome, deseja deletar (s/n)?')
+            if(deletar == 's'):
+                os.remove('')
+                criarArquivo(ano, mes, dia, hor, minu)
+            else:
+                  print('Não podem haver dois arquivos com o mesmo nome. Encerrando programa!')
+                  exit()
+
 def gerarArquivo():
-      diretorio = r'%s' % os.getcwd().replace('\\','/')
-      entrada = open(diretorio, 'w+', encoding = 'UTF-8')
-      obterTexto()
-      entrada.writelines(texto)
-      entrada.close()
+      # diretorio = r'%s' % os.getcwd().replace('\\','/')
+      # entrada = open(diretorio, 'w+', encoding = 'UTF-8')
+      completo = datetime.datetime.now()
+      ano = completo.strftime('%Y')
+      mes = completo.strftime('%m')
+      dia = completo.strftime('%d')
+      hor = completo.strftime('%H')
+      minu = completo.strftime('%m')
+      arquivo = criarArquivo(str(ano), str(mes), str(dia), str(hor), str(minu))
+      arquivo.writelines(obterTexto())
+      arquivo.close()
+      return
 
 if __name__ == '__main__':
-      gerarArquivos()
+      gerarArquivo()
+      exit()
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Códigos Data Science em Python
 <br/>
-Baseado na Playlist: https://www.youtube.com/watch?reload=9&v=HkwX0gB7ZRM&index=2&list=PLxTkH01AauxQclbgvSYLbf3Aya80cKOqa
+Uso de `Numpy` é baseado na playlist do [YouTube](https://www.youtube.com/watch?reload=9&v=HkwX0gB7ZRM&index=2&list=PLxTkH01AauxQclbgvSYLbf3Aya80cKOqa)
 <br/>
-Outras referências: https://stackoverflow.com/questions/3430372/how-to-get-full-path-of-current-files-directory-in-python
+Uso de Jupyter Notebook é baseado no conteúdo da disciplina de [IF-697: Ciência de Dados](https://profluciano.github.io/cd/pages/slides.html)
